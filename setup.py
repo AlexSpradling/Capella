8f963ab5e7eab66258a0b83ee6e7077dba33a118
alex spradling <alexspradling@gmail.com>
2023-11-17 14:23:50 -0500

    version 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Capella',
-    version='2.0',
+    version='2.0.1',
     packages=find_packages(),
     description='A Python package for celestial navigation',
     long_description=open('README.md').read(),
